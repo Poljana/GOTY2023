38b09a0db3cadf350ec2d4d1b7fdfc3e6f4f7b72
Luccci <lucijan.pavin@gmail.com>
2024-01-17 21:59:04 +0100

    modified:   app.js 	modified:   index.css 	modified:   index.html

--- app.js
+++ app.js
@@ -1,27 +1,2 @@
 
-const sidetoggle = document.getElementById('sidebartoggle');
-const sidebar = document.querySelector('sidebar');
 
-sidetoggle.addEventListener('mouseover', function() {
-
-    sidebar.classList.remove("hidden");
-
-});
-
-sidetoggle.addEventListener('mouseout', function() {
-
-    sidebar.classList.add("hidden");
-
-});
-
-sidebar.addEventListener('mouseover', function() {
-
-    sidebar.classList.remove("hidden");
-
-});
-
-sidebar.addEventListener('mouseout', function() {
-
-    sidebar.classList.add("hidden");
-
-});
