c51ee4fc7b1ead14f08955b329e49a8e5d1d206d
Luccci <lucijan.pavin@gmail.com>
2024-01-18 22:36:13 +0100

    renamed:    Images/AWake2_10_08_23_002.jpg -> Images/Alan/AWake2_10_08_23_002.jpg 	renamed:    Images/baldursgate.jpg -> Images/Baldur/baldursgate.jpg 	renamed:    Images/2077.webp -> Images/Cyber/2077.webp 	renamed:    Images/FF16.avif -> Images/FF16/FF16.avif 	renamed:    Images/hifiRush.jpg -> Images/HiFi/hifiRush.jpg 	renamed:    Images/seaofstarscover.jpg -> Images/Stars/seaofstarscover.jpg 	renamed:    Images/tloztotk.webp -> Images/Zelda/tloztotk.webp 	modified:   app.js 	modified:   index.css 	modified:   index.html

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 
 
+function toggleSidebar() {
+    
+    const sidebar = document.getElementById('sidebar');
+    const container = document.querySelector('.container');
+    const sidebarWidth = sidebar.offsetWidth;
+
+    if (sidebar.style.transform === 'translateX(0em)') {
+
+        sidebar.style.transform = `translateX(-${sidebarWidth}em)`;
+        container.style.transform = 'translateX(0px)';
+        container.style.filter = 'brightness(1)';
+
+    } else {
+
+        sidebar.style.transform = 'translateX(0em)';
+        container.style.transform = `translateX(${sidebarWidth}px)`;
+        container.style.filter = 'brightness(0.7)';
+    
+    }
+
+}
